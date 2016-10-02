cc6fe8ab4e4666dd8f4aea7357808edbf390f17d
rafael <rafael@gmail.com>
2016-10-01 17:15:39 -0700

    Se creo la clase empleado

---
+++ empleado.java
@@ -0,0 +1,67 @@
+package practica4;
+
+public class empleado {
+	private String nombre;
+        private int edad, horasTrabajadas;
+	private float salario;
+	private String apellidoMaterno, apellidoPaterno;
+
+    public empleado() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if(edad > 0 && edad <= 100) {
+            this.edad = edad;
+        }
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    
+    public float sueldo() {
+    	float sueldo=getSalario()*getHorasTrabajadas();
+    	return sueldo;
+    }
+}
